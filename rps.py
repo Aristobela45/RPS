9ba3a4902f9db458ad3903d7b1eb8eed8d1b1a23
Swan Erickson-Gunther <SwanEricksonGunther@gmail.com>
2023-03-24 15:44:53 -0500

    first commit

--- rps.py
+++ rps.py
@@ -8,6 +8,7 @@
 
 # Continue to ask the player for their input until they say "I quit", at which time the game will then end and display ("Thank you for playing").
 
+print("Hello, and welcome to COMPUTERIZED ROCK-PAPER-SCISSORS v1.0")
 while True:
     #Ask the player to select a move and store it.
     player= input("Choose 'Rock', 'Paper', 'Scissors', or type 'quit'. ").lower()
@@ -16,22 +17,10 @@ while True:
     
     #If player chooses to quit, the game will end and display ("Thank you for playing").
          
-    
     #Computer chooses an option after the player and store it. 
     
     #Both selections will be compared.
    
     #If selections are the same, print it's a tie.
-   
-    #If player chooses rock and computer chooses scissors, the player wins.
-  
-    #If player chooses rock and computer chooses paper, the player loses the game.
-   
-    #If player chooses scissors and computer chooses paper, the player wins.
-    
-    #If player chooses scissors and computer chooses rock, the player loses.
-   
-    #If player chooses paper and computer chooses rock, the player wins.
-   
-    #If player chooses paper and computer chooses scissors, the player loses. 
+
 
