25e15913846bf5cafcc3e81b6d8cedcc8f2179df
PatrickTali <ptaliaferro218@gmail.com>
2023-03-24 13:01:15 -0400

    wp2

---
+++ rps.py
@@ -0,0 +1,37 @@
+# If player and computer choose the same decision then display ("Game Tied"), 
+
+
+# If the player chooses "Rock" and the computer chooses "Paper" display ("You lose"), 
+# If the player chooses "Scissors" and the computer chooses "Rock" display ("You Lose")  and 
+# If the player chooses "Paper" and the computer chooses "Scissors" then display ("You lose") 
+# -- Vice versa for other decisions.
+
+# Continue to ask the player for their input until they say "I quit", at which time the game will then end and display ("Thank you for playing").
+
+while True:
+    #Ask the player to select a move and store it.
+    player= input("Choose 'Rock', 'Paper', 'Scissors', or type 'quit'. ").lower()
+    cpu=""
+
+    
+    #If player chooses to quit, the game will end and display ("Thank you for playing").
+         
+    
+    #Computer chooses an option after the player and store it. 
+    
+    #Both selections will be compared.
+   
+    #If selections are the same, print it's a tie.
+   
+    #If player chooses rock and computer chooses scissors, the player wins.
+  
+    #If player chooses rock and computer chooses paper, the player loses the game.
+   
+    #If player chooses scissors and computer chooses paper, the player wins.
+    
+    #If player chooses scissors and computer chooses rock, the player loses.
+   
+    #If player chooses paper and computer chooses rock, the player wins.
+   
+    #If player chooses paper and computer chooses scissors, the player loses. 
+
